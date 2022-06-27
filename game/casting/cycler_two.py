57639bf92a8fd548557ef3a8e63a1c7e6ec61579
Calebfinn143 <bar17106@byui.edu>
2022-06-27 02:12:41 -0600

    Update coloring for cyclers

--- game/casting/cycler_two.py
+++ game/casting/cycler_two.py
@@ -38,14 +38,14 @@ class cycler_two(Actor):
         self._segments[0].set_velocity(velocity)
     
     def _prepare_body(self):
-        x = int(constants.MAX_X / 2)
+        x = 600
         y = int(constants.MAX_Y / 2)
 
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.GREEN
+            color = constants.GREEN
             
             segment = Actor()
             segment.set_position(position)
